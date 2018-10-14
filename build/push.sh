49cf7468e069c6263efec44becde9edde6695872
ottah <357850+ottah@users.noreply.github.com>
2018-10-14 04:43:13 +0100

    Build test

---
+++ build/push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+branch_name=`git rev-parse --abbrev-ref HEAD`
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git checkout -b branch_name
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  branch_name=`git rev-parse --abbrev-ref HEAD`
+  git push origin $branch_name
+  git remote add $branch_name https://${GH_TOKEN}@github.com/ottah/ArtifactDB.git > /dev/null 2>&1
+  git push --quiet --set-upstream $branch_name 
+}
+
+
+setup_git
+commit_files
+upload_files
