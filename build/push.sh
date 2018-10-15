fa991aace0b5b35ef85edb1f11f7d2b33fc470c9
ottah <357850+ottah@users.noreply.github.com>
2018-10-15 01:46:57 +0100

    push

--- build/push.sh
+++ build/push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 setup_git() {
-  git config user.email "travis@travis-ci.org"
-  git config user.name "Travis CI"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
 }
 
 commit_files() {
